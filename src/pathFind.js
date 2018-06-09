5ca3af44c6d66b3ec81837b35db9779a8afd3665
Addison Schultz <addisonschultz0@gmail.com>
2018-06-09 12:33:12 +0200

    working for multiple strings, no recursion

--- src/pathFind.js
+++ src/pathFind.js
@@ -1,63 +1,62 @@
 // path = array of 1 string // EXAMPLE ['foo']
 // object = object // EXAMPLE { name: 'foo', age: 2 }
+// Call the function pathFind
 
-const obj = {
-  foo: "Hey"
-}
 
-function pathFind(path, object) {
-
+// Only is working for one string in array :(
+function myPathFind(path, object) {
   const reducer = path.reduce(function(acc, current){
     return acc + current
   }, '')
   return object[reducer]
-
 }
 
-// pathFind(['foo'], obj)
 
-
-
+// Will work for either multiple or single string arrays!! :)
+const pathFind = (path, obj) => {
+    return path.reduce((object, key) =>
+        (object && object[key] !== 'undefined') ? object[key] : undefined, obj);
+}
 
 
 
 // USING BELOW THIS LINE TO TEST
 
 
-const oneString = ['foo1']
-const multipleString = ['foo1', 'foo2', 'bar']
+// const oneString = ['foo']
+// const multipleString = ['foo1', 'foo2', 'bar']
+//
+// const objOne = {
+//   foo: "Hey"
+// }
+//
+// const objMultiple = {
+//     foo1: {
+//         foo2: {
+//             bar: "Some value"
+//         }
+//     }
+// }
 
-const objTest = {
-    foo1: {
-        foo2: {
-            bar: "Some value"
-        }
-    }
-}
+// function imATester(array, object){
+//     if ( array.length <= 1 ) {
+//       const reducer = array.reduce(function(acc, current){
+//         return acc + current
+//       }, '')
+//       return object[reducer] //THIS WILL WORK FOR ONE STRING
+//     } else {
+//
+//       const result = array.reduce((obj, key) =>
+//         (obj && obj[key] !== 'undefined') ? obj[key] : undefined, object);
+//         return result
+//     }
+// }
 
-console.log()
-
-
-function imATester(array, object){
-
-    if ( array.length <= 1 ) {
-      const reducer = array.reduce(function(acc, current){
-        return acc + current
-      }, '')
-      console.log(reducer)
-      // return object[reducer] //THIS WILL WORK FOR ONE STRING
-    } else {
-      const reducer = array.map(item => item)
-      console.log(reducer)
-    }
-
-}
-
-// imATester(oneString, objTest)
-// imATester(multipleString, objTest)
-
-
-
+// const one = imATester(objOne, oneString);
+// console.log(one)
+//
+// const multiple = imATester(objMultiple, multipleString);
+// console.log(multiple)
 
 
 module.exports = { pathFind }
